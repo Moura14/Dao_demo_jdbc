4ad410af45bcdf4c9d408d95605f3c9fa7e30a6b
João Gabriel <joaogabrielmoura14@gmail.com>
2022-04-13 16:03:23 -0300

    insert implementation

--- src/model/entites/Seller.java
+++ src/model/entites/Seller.java
@@ -39,21 +39,27 @@ public class Seller implements Serializable {
 		this.id = id;
 	}
 
+
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 
 	public Date getBirthDate() {
 		return birthDate;
